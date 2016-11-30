9d02cf337ed0e6325e8087883f5cb787e5ac281e
Dilip Kumar <dilip.programmer@gmail.com>
2016-11-30 02:46:25 -0800

    Changing the screen background, image and title based on the selection

--- app/src/main/java/com/flaborfit/dilip/flaborfit/DetailActivity.java
+++ app/src/main/java/com/flaborfit/dilip/flaborfit/DetailActivity.java
@@ -1,8 +1,11 @@
 package com.flaborfit.dilip.flaborfit;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -13,8 +16,24 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         TextView textTitle = (TextView) findViewById(R.id.text_title);
-
         ImageView imageMain = (ImageView) findViewById(R.id.image_main);
+        LinearLayout linearBackground = (LinearLayout) findViewById(R.id.linearlayout_main_background);
+
+        //Get data from the first activity
+        String exerciseText = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        textTitle.setText(exerciseText);
+
+        if(exerciseText.equalsIgnoreCase(MainActivity.EXERCISE_CARDIO)){
+            linearBackground.setBackgroundColor(Color.parseColor("#57b25c"));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                imageMain.setImageDrawable(getResources().getDrawable(R.drawable.heart,getApplicationContext().getTheme()));
+            }
+        }else if(exerciseText.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)){
+            linearBackground.setBackgroundColor(Color.parseColor("#926cce"));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                imageMain.setImageDrawable(getResources().getDrawable(R.drawable.lotus,getApplicationContext().getTheme()));
+            }
+        }
 
     }
 }
